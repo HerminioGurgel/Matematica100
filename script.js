c3910702cdf5a71e0940835b15b1bc992b7578c6
HerminioGurgel <herminio.gurgel@gmail.com>
2021-10-29 11:21:14 -0300

    Mecânica básica funcionando
    Até esse ponto o jogo já detecta as respostas e rotaciona o painel com as próximas contas, mas sem muito funcionalidade do jogo em si implementada.

--- script.js
+++ script.js
@@ -1,58 +1,89 @@
 /*
-A função focar tem o único objetivo de "começar" o jogo e
-mudar o foco do cursor do mouse para o input resposta
-Essa função é chamada automátcamente no Onload do body
+Variáveis do jogo
 */
-function focar(){
-    var a = document.querySelector('input#resposta')
-    a.focus()
-    gerarContas()
+let a = document.querySelector('input#resposta')
+let resolvido = document.querySelector('input#resolvido')
+let atual = document.querySelector('input#atual')
+let proxima = document.querySelector('input#proximo')
+let respostaUsr = document.querySelector('input#resposta')
+let rodada = 0
+let questoes = []
+
+let objSoma = {
+    parcelaX: 0,
+    parcelaY: 0,
+    soma: 0,
+
+    geraConta() {
+        this.parcelaX = this.geraNumero(0, 10)
+        this.parcelaY = this.geraNumero(0, 10)
+        this.soma = this.parcelaX + this.parcelaY
+    },
+
+    geraNumero(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    },
+
+    escreveConta() {
+        return `${this.parcelaX} + ${this.parcelaY}`
+    }
 }
 
-function gerarContas(){
+function focar() {
+    a.focus()
+    a.value = ""
+}
 
-    let contas = [] //Array onde são armazenados objetos do tipo soma
-    /*
-    Objeto soma, gera duas parcelas e o resultado da soma
-    Ainda falta melhorar a parte de OO, ta completamente na gambiarra
-    */
-    let soma = {
-        parcelaX: 0,
-        parcelaY: 0,
-        soma: 0,
-    
-        //Função geraConta inicializa as variáveis do objeto
-        geraConta(){
-            this.parcelaX = this.geraNumero(0, 21)
-            this.parcelaY = this.geraNumero(0, 21)
-            this.soma = this.parcelaX + this.parcelaY
-        },
-        //Função geraNúmero é uma função para gerar dois números aleatórios
-        geraNumero(min, max){
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min)) + min;
-        },
-        //Tem a única função de escrever os dados onde vai ser apresentado no programa
-        escreveConta(){
-            return `${this.parcelaX} + ${this.parcelaY}`
+function iniciar(){
+    gerarContas()
+    focar()
+}
+
+/*
+Cria os array de questões e preenche ele com objSoma
+*/
+function gerarContas() {
+    let conta
+    for (let index = 0; index < 100; index++) {
+        conta = Object.create(objSoma)
+        conta.geraConta()
+        questoes.push(conta)
+    }
+    resolvido.value = ``
+    atual.value = `${questoes[0].escreveConta()}`
+    proxima.value = `${questoes[1].escreveConta()}`
+}
+
+/*
+Função que checa se a resposta que o usuário digitou está certa ou errada
+Falta corrigir uma falha o maxlength do input respostaUsr que está causando uma pequena falha
+*/
+function checaResposta() {
+    let tamanhoSoma = questoes[rodada].soma.toString().length
+    respostaUsr.maxlength = tamanhoSoma
+    let tamanhoResUsr = respostaUsr.value.toString().length
+
+    if (tamanhoSoma == tamanhoResUsr) {
+
+        if (respostaUsr.value == questoes[rodada].soma) {
+            trocaQuestao()
+        } else {
+            trocaQuestao()
         }
-    } // Objeto soma acaba aqui
-    
-    //Isso aqui ainda vi ser colocado em um loop para preencher o array
-    soma.geraConta()
-    contas.push(soma)
-    soma.geraConta()
-    contas.push(soma)
+        focar()
+    }
+}
 
-    /*
-    Essas variáveis representam os campos onde vão aparecer as repostas
-    */
-    let resolvido = document.querySelector('input#resolvido')
-    let atual = document.querySelector('input#atual')
-    let proxima = document.querySelector('input#proximo')
-
-    resolvido.value=``
-    atual.value=`${contas[0].escreveConta()}`
-    proxima.value=`${contas[1].escreveConta()}`
+/*
+Função que rotaciona o painel lateral de questões
+Falta adicionar a resposta dada pelo usuário, já mostrando certo ou errado e adicionar um som que será tocado em caso de acerto ou erro
+*/
+function trocaQuestao(){
+    rodada++
+    resolvido.value = `${questoes[rodada - 1].escreveConta()}`
+    atual.value = `${questoes[rodada].escreveConta()}`
+    proxima.value = `${questoes[rodada + 1].escreveConta()}`
+    focar()
 }
